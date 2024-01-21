8afef94bbcd238761c1f0e7a1a156c75057b5729
Abdul Haseeb <abdulhaseeb.9820@gmail.com>
2024-01-22 01:48:59 +0500

    bool type_cast

--- type_cast.dart
+++ type_cast.dart
@@ -1,6 +1,23 @@
 void main(List<String> args) {
   var number = 10;
 
-  print(number.toDouble()); // this will make int to double only in this line
-  print(number);
+  print(
+      "var value ${number}"); // this will make int to double only in this line
+
+  print(
+      "var value runtime type: ${number.runtimeType}"); // this will make int to double only in this line
+  print("====================");
+  print(
+      "double value ${number.toDouble()}"); // this will make int to double only in this line
+  print(
+      "double value runtime type: ${number.toDouble().runtimeType}"); // this will make int to double only in this line
+
+  var booleanValue = true;
+  print("====================");
+  print("boolean value: ${booleanValue}");
+  print("boolean value runtime type: ${booleanValue.runtimeType}");
+  print("====================");
+  print("type casted value: ${booleanValue.toString()}");
+  print(
+      "type casted value runtime type: ${booleanValue.toString().runtimeType}");
 }
